1506d40f1524924572c6436afd31fd14600115f5
Sergey26-zh <ivahnenkose.2003@gmail.com>
2025-04-20 21:50:28 +0700

    переделал дто запроса

---
+++ src/main/java/com/example/learntodoback/dto/schema/TargetContactDto.java
@@ -0,0 +1,12 @@
+package com.example.learntodoback.dto.schema;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TargetContactDto {
+    private String id;
+    private int x;
+    private int y;
+}
